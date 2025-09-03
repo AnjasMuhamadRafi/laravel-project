83b32e8d9647a9a76b59c409533b312ed9fd784d
AnjasMuhamadRafi <anjas5rafi45@gmail.com>
2025-09-03 11:34:32 +0700

    Update Logo Enlulu

--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -12,6 +12,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PT. ENLULU SUKSES MAKMUR - Professional Outsourcing Solutions</title>
+    <link rel="icon" type="image/png" href="assets/images/enlulu.png">
+    <link rel="shortcut icon" type="image/png" href="assets/images/enlulu.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
